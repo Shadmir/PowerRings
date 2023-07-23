ba8f5d73a3b27e8586ca98b436e0a321e5fb4dd5
David <david@tynobryn.co.uk>
2023-07-23 22:20:30 +0100

    Added ring?

--- src/main/java/com/shadmir/powerrings/items/items.java
+++ src/main/java/com/shadmir/powerrings/items/items.java
@@ -1,5 +1,6 @@
 package com.shadmir.powerrings.items;
 
+import com.shadmir.powerrings.Tags;
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
@@ -8,6 +9,7 @@ public class items {
     public static Item basicRing;
     public static final void init() {
         basicRing = new Item().setUnlocalizedName("basicRing").setCreativeTab(CreativeTabs.tabCombat);
+        basicRing.setTextureName(Tags.MODID + ":" + basicRing.getUnlocalizedName());
         GameRegistry.registerItem(basicRing, basicRing.getUnlocalizedName());
     }
 
